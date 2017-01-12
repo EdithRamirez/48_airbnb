daf76d4b95fa380176cffe7689979b0be722bc98
EdithRamirez <ediths.ramirez07@gmail.com>
2017-01-12 15:58:16 -0600

    agregue más estilos a index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-	//intervalo de imagenes de slider
+	//cambiar color de menu al hacer scroll
+	var position = $('#myCarousel').position().top;
+	var borde = $('.navbar-brand');
+
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > position) {
+			console.log(position);
+			$('nav').addClass('nav-blanco');
+			$(borde).addClass('borde');
+		} else {
+			$('nav').removeClass('nav-blanco');
+			$(borde).removeClass('borde');
+		}
+	});
+	
+	//intervalo de imagenes de slid
 	$('.carousel').carousel({
 		interval: 5000
 	});
-
-	//datepicker
+	
+	//datepicker calendario
 	$(function() {
 		//fecha de llegada
 		$('#dateEntrada').datepicker();
 		//fecha de salida
 		$('#dateSalida').datepicker();
 	});
+
+	// $('input').on('click', function() {
+	// 	$(this).css("border-bottom", "1px solid green")
+	// });
 });
